1ee53905078878c3b1dd8365abff7c3ef3b16282
Joop Kiefte <joop@kiefte.net>
2015-05-14 00:40:19 +0200

    Oops, again path wrong... test better please ;)

--- krei_mobi.bash
+++ krei_mobi.bash
@@ -2,7 +2,7 @@
 ebook-convert --version || ( echo "Vi bezonas havi calibre instalita kaj ebook-convert en via PATH" ; exit 1 )
 mkdir mobi
 cd epub
-for file in *.epub; do ebook-convert "$file" "../mobi/$file.mobi" --output-profile=kindle; mutt $1 -a "mobi/$file.mobi" -s "" <<<""; done
+for file in *.epub; do ebook-convert "$file" "../mobi/$file.mobi" --output-profile=kindle; mutt $1 -a "../mobi/$file.mobi" -s "" <<<""; done
 cd ..
 rename "s/.epub.mobi/.mobi_/" mobi/*.mobi
 rm mobi/*.mobi
