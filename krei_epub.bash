3d30fcd4e6562b8ef54c84f18cef406c22a42d68
Joop Kiefte <joop@kiefte.net>
2015-05-14 00:28:25 +0200

    Correct scripts to remove old files, to support direct mailing and to generate correct titles in the mobi files for Kindle usage

--- krei_epub.bash
+++ krei_epub.bash
@@ -2,4 +2,6 @@
 pandoc --version || ( echo "Vi bezonas havi pandoc instalita kaj en via PATH" ; exit 1 )
 mkdir epub
 for file in *.md; do [ "x$file" != "xREADME.md" ] && pandoc "$file" postglui.text -o "epub/$file.epub"; echo Faris $file; done
-rename "s/.md.epub/.epub/" epub/*.epub
+rename "s/.md.epub/.epub_/" epub/*.epub
+rm epub/*.epub
+rename "s/.epub_/.epub/" epub/*.epub_
